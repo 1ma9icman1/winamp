47e8bb4d1812b0646303ad9d7e3e70107e5d1db2
ma9icman1 <ma9icfam@gmail.com>
2026-09-08 02:07:11 -0400

    fix: import Buffer to resolve Vercel build error

--- api/remote-skin/[hash].ts
+++ api/remote-skin/[hash].ts
@@ -1,3 +1,5 @@
+import { Buffer } from 'node:buffer'
+
 export default async function handler(request: any, response: any) {
   const hash = request.query.hash
   const upstream = await fetch(`https://r2.webampskins.org/skins/${encodeURIComponent(hash)}.wsz`)
