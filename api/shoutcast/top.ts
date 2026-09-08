82edb2e01e68897083556a9c591b566e7bf87f44
ma9icman1 <ma9icfam@gmail.com>
2026-09-08 00:40:08 -0400

    Forward top stations request as POST

--- api/shoutcast/top.ts
+++ api/shoutcast/top.ts
@@ -1,5 +1,7 @@
 export default async function handler() {
-  const response = await fetch('https://directory.shoutcast.com/Home/Top')
+  const response = await fetch('https://directory.shoutcast.com/Home/Top', {
+    method: 'POST',
+  })
   return new Response(await response.text(), {
     status: response.status,
     headers: { 'Content-Type': response.headers.get('Content-Type') || 'application/json' },
